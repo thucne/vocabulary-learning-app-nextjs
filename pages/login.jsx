3e2ae8a734b61e379c3345a4c76182a100e8e168
KhangDuy <nguyenduykhangcd12t4@gmail.com>
2022-02-16 16:06:16 +0700

    auth page

---
+++ pages/login.jsx
@@ -0,0 +1,15 @@
+import Login from '@components/Auth/Login'
+import React from 'react'
+
+/**
+* @author
+* @function LoginPage
+**/
+
+export default function LoginPage  (props){
+  return(
+    <div>
+        <Login/>
+    </div>
+   )
+  }
