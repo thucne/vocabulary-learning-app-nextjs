024005d161a6fa37e5d758e915926f81ea535575
KhangDuy <nguyenduykhangcd12t4@gmail.com>
2022-02-18 15:02:49 +0700

    dashboard

---
+++ components/Dashboard/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Welcome from './Welcome'
+
+/**
+* @author
+* @function DashboardPage
+**/
+
+export default function DashboardPage (props){
+  return(
+    <div>
+        <Welcome/>
+    </div>
+   )
+  }
